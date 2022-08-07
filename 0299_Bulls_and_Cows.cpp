20e8ffdaf7afd21e0954325c10437dbb84d9f50d
Daniel Chang <r04945022@ntu.edu.tw>
2022-08-07 15:06:57 +0800

    Update 0299_Bulls_and_Cows.cpp

--- 0299_Bulls_and_Cows.cpp
+++ 0299_Bulls_and_Cows.cpp
@@ -27,3 +27,36 @@ public:
         return res;
     }
 };
+
+
+// one pass solution
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int countA = 0, countB = 0;
+        unordered_map<char, int> st;
+        string res = "";
+        for(int i = 0; i < secret.size(); i++)
+        {
+            if(guess[i] == secret[i])
+            {
+                countA++;
+            }
+            else
+            {
+                if(st[guess[i]]-- > 0 )
+                {
+                    countB++;
+                    //st[guess[i]]--;
+                }
+                if( st[secret[i]]++ < 0 )
+                {
+                    countB++;
+                    //st[secret[i]]++;
+                }
+            }
+        }
+        res = to_string(countA) + "A" + to_string(countB) + "B";
+        return res;
+    }
+};

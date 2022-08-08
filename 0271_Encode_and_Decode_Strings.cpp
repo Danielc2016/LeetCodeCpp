bda5f972c66ba18cf57ae486b4eacb98805b2bf4
Daniel Chang <r04945022@ntu.edu.tw>
2022-08-08 10:06:10 +0800

    Update 0271_Encode_and_Decode_Strings.cpp

--- 0271_Encode_and_Decode_Strings.cpp
+++ 0271_Encode_and_Decode_Strings.cpp
@@ -32,3 +32,47 @@ public:
 // Your Codec object will be instantiated and called as such:
 // Codec codec;
 // codec.decode(codec.encode(strs));
+
+
+// Chunk encoding
+const int HEADER = 4;
+class Codec {
+
+private:
+    string int2string( int num )
+    {
+        stringstream ss;
+        ss << setw(HEADER) << setfill('0') << num;
+        return ss.str();
+    }
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs)
+    {
+        string res = ""; 
+        for(auto s : strs)
+        {
+            int iLen = s.length();
+            string sLen = int2string(iLen);
+            res += (sLen + s);
+        }
+        return res;
+    }
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s)
+    {
+        vector<string> res;
+        while(s.length() > 0)
+        {
+            string sLen = s.substr(0, HEADER);
+            int iLen = stoi(sLen);
+            s.erase(0, HEADER);
+            string str = s.substr(0, iLen);
+            s.erase(0, iLen);
+            res.push_back(str);
+        }
+        return res;
+    }
+};
+

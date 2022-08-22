a19f9ca2d3b4c1ca0793d389540111472ce1ab0d
Daniel Chang <r04945022@ntu.edu.tw>
2022-08-22 10:17:40 +0800

    Create 0490_The_Maze.cpp

---
+++ 0490_The_Maze.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) 
+    {
+        bool res = false;
+        vector<vector<int>> visited( maze.size(), vector<int>(maze[0].size(), 0));
+        visited[start[0]][start[1]] = 1;
+        walk(maze, start[0], start[1], visited, destination, res);
+        return res;
+    }
+    
+
+    void walk(vector<vector<int>>& maze, int i, int j,
+              vector<vector<int>>& visited,
+              vector<int>& destination, bool& pass)
+    {
+        //exit statement
+        // if(i < 0 || i >= maze.size() || j < 0 || j >= maze[0].size() )
+        //     return;
+        if(i == destination[0] && j == destination[1] )
+        {
+            pass = true;
+            return;
+        }
+        vector<vector<int>> dirs_ = {{-1,0},{0,1},{1,0},{0,-1}};
+        for(auto& ele : dirs_)
+        {
+            int x = i, y = j;
+            while( x>=0 && x < maze.size() &&
+               y >= 0 && y < maze[0].size() && !maze[x][y])
+            {
+                x += ele[0];
+                y+=ele[1];
+            }
+            x-=ele[0];
+            y-=ele[1];
+            if(!visited[x][y])
+            {
+                visited[x][y] = 1;
+                walk(maze, x, y, visited, destination, pass);
+            }
+        }
+    }
+};

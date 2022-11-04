81c7b8e6f7b13a87d5dc93e19fd75bec4b5b1a9c
Daniel Chang <r04945022@ntu.edu.tw>
2022-11-04 11:17:58 +0800

    Update 0076_Minimum_Window_Substring.cpp

--- 0076_Minimum_Window_Substring.cpp
+++ 0076_Minimum_Window_Substring.cpp
@@ -28,3 +28,52 @@ public:
         return minLen == INT_MAX ? "" : s.substr(minStart, minLen);
     }
 };
+
+// 11/04 re-challenge:
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> tmap;
+        string res = "";
+        int minLen = INT_MAX;
+        int count = 0;
+        int left = 0, right = 0;
+        for(auto& c : t)
+        {
+            tmap[c]++;
+        }
+        while(right < s.length())
+        {
+            if( tmap[s[right]] > 0 )
+                count++;
+            tmap[s[right]]--;
+            if( count == t.length() )
+            {
+                while(left < right && tmap[s[left]] < 0)
+                {
+                    tmap[s[left++]]++;
+                }
+                if(minLen > right - left + 1)
+                {
+                    minLen = right - left + 1;
+                    res = s.substr(left, minLen);
+                }
+                tmap[s[left++]]++;
+                count--;
+            }
+            right++;
+        }
+        return minLen == INT_MAX ? "" : res ;
+    }
+};
+/*
+s = ADOBECODEBANC
+t = ABC
+   l
+s = ADOBECODEBANC
+         r
+
+
+
+*/
